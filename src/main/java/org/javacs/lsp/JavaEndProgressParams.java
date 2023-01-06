ba74657eca07d3394877f0f727be6274d06f0dc5
Trey Wood <treyw@squareup.com>
2023-01-05 23:30:19 -0700

    refactor

--- src/main/java/org/javacs/lsp/JavaEndProgressParams.java
+++
@@ -1,7 +0,0 @@
-package org.javacs.lsp;
-
-public class JavaEndProgressParams extends JavaProgressParams {
-  public JavaEndProgressParams(String token, String message) {
-    super("end", token, message);
-  }
-}
--- src/main/java/org/javacs/lsp/JavaStartProgressParams.java
+++
@@ -1,7 +0,0 @@
-package org.javacs.lsp;
-
-public class JavaStartProgressParams extends JavaProgressParams {
-  public JavaStartProgressParams(String token, String message) {
-    super("begin", token, message);
-  }
-}
